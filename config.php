97a40c27406790f68ec5bb03e4e2e51e02424a2d
Belén Valencia <belenvalenciasaiz@gmail.com>
2017-01-31 12:59:51 +0100

    Inicial

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+	$host = "localhost";  //conexion al hosting
+	$db = "ctres_reservas";  //nombre de la base de datos
+	$user = "c3admin"; //nombre de usuario
+	$pass = "CtR3sPo";  //clave de usuario
+
+	$conexion = mysqli_connect($host, $user, $pass) or die('No se pudo conectar con la base de datos: ' . mysqli_error());
+
+	mysqli_select_db($conexion, $db) or die('No se pudo seleccionar la base de datos');
+
+    if ($conexion){echo ("Todo va bien, todo es fabuloso");}
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include "config.php" ?>
+
+<!DOCTYPE html>
+
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/reset.css" type="text/css" />
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<script src="js/script.js" type="text/javascript"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	
+	<title>C3PO</title>
+</head>
+
+<body>
+    
+</body>
+    
+</html>
