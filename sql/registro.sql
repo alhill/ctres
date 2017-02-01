cd776d439384e7e376f802a68397ff45aabb86d6
alhill <alvarogil91@gmail.com>
2017-02-01 09:50:07 +0100

    Tabla 'registro'

--- sql/registro.sql
+++ sql/registro.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-01-2017 a las 22:16:01
+-- Tiempo de generación: 01-02-2017 a las 09:49:08
 -- Versión del servidor: 10.1.19-MariaDB
 -- Versión de PHP: 7.0.13
 
@@ -19,33 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ctres_reservas`
 --
--- CREATE DATABASE `ctres_reservas` COLLATE utf8_spanish_ci;
--- USE `ctres_reservas`;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `lista_reservas`
---
-
-CREATE TABLE `lista_reservas` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `title` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `body` text COLLATE utf8_spanish_ci NOT NULL,
-  `url` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
-  `class` varchar(45) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'event-important',
-  `start` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
-  `end` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
-  `inicio_normal` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `final_normal` varchar(50) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `lista_reservas`
---
-
-INSERT INTO `lista_reservas` (`id`, `title`, `body`, `url`, `class`, `start`, `end`, `inicio_normal`, `final_normal`) VALUES
-(3, '', ' ', 'http://localhost/calendario/descripcion_evento.php?id=3', '02', '1487159400000', '1487163000000', '15/02/2017 12:50', '15/02/2017 13:50');
 
 -- --------------------------------------------------------
 
@@ -53,6 +26,7 @@ INSERT INTO `lista_reservas` (`id`, `title`, `body`, `url`, `class`, `start`, `e
 -- Estructura de tabla para la tabla `registro`
 --
 
+DROP TABLE IF EXISTS `registro`;
 CREATE TABLE `registro` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
@@ -72,17 +46,11 @@ INSERT INTO `registro` (`id`, `nombre`, `apellido`, `usuario`, `email`, `contras
 (2, 'testdos', 'tester', 'test2', 'test2@email.com', '098f6bcd4621d373cade4e832627b4f6', '1'),
 (3, 'Alvaro', 'Gil', 'alhill', 'alvarogil91@gmail.com', 'efe6398127928f1b2e9ef3207fb82663', '3'),
 (4, 'seÃ±or', 'sin privilegios', 'usuario', 'usuario@raso.net', '81dc9bdb52d04dc20036dbd8313ed055', '1'),
-(5, 'un', 'propietario', 'propietario', 'propietario@dela.sala', '81dc9bdb52d04dc20036dbd8313ed055', '0');
+(5, 'un', 'propietario', 'propietario', 'propietario@dela.sala', '81dc9bdb52d04dc20036dbd8313ed055', '2');
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `lista_reservas`
---
-ALTER TABLE `lista_reservas`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `registro`
@@ -94,11 +62,6 @@ ALTER TABLE `registro`
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
---
--- AUTO_INCREMENT de la tabla `lista_reservas`
---
-ALTER TABLE `lista_reservas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `registro`
 --
