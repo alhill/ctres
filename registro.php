2b0e4de9e74799bfcb1cea0138be029b8975576b
angelma83 <angelma83@gmail.com>
2017-02-06 14:25:09 +0100

    registro con validacion php

--- registro.php
+++ registro.php
@@ -72,21 +72,56 @@
 </div>
     
 <?php
-        if (isset($_POST['insertar'])) {
-        $nombre = mysqli_real_escape_string ($conexion, $_POST['nombre']); // mysqli_real_escape_string ($con,...) EVITA INYECCIONES SQL
-        $apellido = mysqli_real_escape_string ($conexion, $_POST['apellido']);
-        $usuario = mysqli_real_escape_string ($conexion, $_POST['usuario']);
-        $email = mysqli_real_escape_string ($conexion, $_POST['email']);
-   
-        $contrasena = mysqli_real_escape_string ($conexion, $_POST['contrasena']);
-        $contrasena2 = mysqli_real_escape_string ($conexion, $_POST['contrasena2']);
-        $privilegios = mysqli_real_escape_string ($conexion, $_POST['privilegios']);
 
-     	if($contrasena!=$contrasena2) {
-				echo "Las contraseñas no coinciden";
-			}else {
+include 'config.php';
 
+if (isset($_POST['insertar'])) {
+
+		$nombre = mysqli_real_escape_string ($conexion, $_POST['nombre']); // mysqli_real_escape_string ($con,...) EVITA INYECCIONES SQL
+		$apellido = mysqli_real_escape_string ($conexion, $_POST['apellido']);
+		$usuario = mysqli_real_escape_string ($conexion, $_POST['usuario']);
+		$email = mysqli_real_escape_string ($conexion, $_POST['email']);
+		$contrasena = mysqli_real_escape_string ($conexion, $_POST['contrasena']);
+		$contrasena2 = mysqli_real_escape_string ($conexion, $_POST['contrasena2']);
+		
+		if (isset($_POST['privilegios'])){
+			$privilegios = mysqli_real_escape_string ($conexion, $_POST['privilegios']);
+		}else{
+			$privilegios = 1;
+		}
+        //Pasamos a validar el formulario de registro
+	    if(empty($_POST["nombre"])){
+	        $errores[] = "El nombre es requerido";
+	    }
+
+	    if(empty($_POST["apellido"])){
+	       $errores[] = "El apellido es requerido";
+	    }
+
+	    if(empty($_POST["usuario"])){
+	        $errores[] = "El usuario es requerido";
+	    }
+
+	    if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) || empty($_POST["email"])){
+	         $errores[] = "El email es requerido";
+	    }
+
+	    if(empty($_POST["contrasena"]) || strlen($_POST["contrasena"]) != 5){
+	      $errores[] = "La contraseña debe contener 5 caracteres";
+	    }
+
+	    if(empty($_POST["contrasena2"]) || strlen($_POST["contrasena2"]) != 5){
+	      $errores[] = "La contraseña debe contener 5 caracteres";
+	    }
+
+     	if($contrasena != $contrasena2) {
+			$errores[] = "Las contraseñas no coinciden";
+		}else{
 				$contrasena = md5($_POST['contrasena']);
+		}
+		
+		if(empty($errores)) {
+
 				$buscarUser = "SELECT * FROM registro WHERE usuario ='$usuario' OR email ='$email'";
 				$ejecutar= mysqli_query($conexion, $buscarUser);
 				$check = mysqli_num_rows($ejecutar);
@@ -94,24 +129,36 @@
 				if ($check>0) {
 
 					echo "<script>
-					alert('El usuario o email ya existe. Vuelve a intentarlo');
-					history.back();
-					</script>";
+						alert('El usuario o email ya existe. Vuelve a intentarlo');history.back();
+						</script>";
 				}else{
 
-				//Todo parece correcto procedemos con la insercion
+		        //Todo parece correcto procedemos con la insercion
+
 				$query = "INSERT INTO registro (nombre, apellido, usuario, email, contrasena, privilegios) VALUES ('$nombre','$apellido', '$usuario', '$email','$contrasena', '$privilegios')";
 				
 				$ejecutar = mysqli_query($conexion, $query) or die('Hubo un error durante el registro: ' . mysqli_error($conexion));
                     
-			
 				echo "<script> alert('El registro se completó correctamente');
-					 window.open('calendario_index.php','_self');  
+					 window.open('index.php','_self');
 					 </script>";
-			} //OJOOO
+			
+				}
+			}
+		}
+?>
+
+<ul>
+	<?php if(isset($errores)){
+		foreach ($errores as $error){
+			echo "<li> $error </li>";
 		}
 	}
-?>
+	?>
+</ul>
+
+
+
 
 </body>
 </html>
